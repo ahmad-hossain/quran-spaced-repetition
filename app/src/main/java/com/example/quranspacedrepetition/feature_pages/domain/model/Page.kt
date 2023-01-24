c7b027e1d630500b17d8b453f310038028638795
Name <ahmad.awaab@gmail.com>
2023-01-24 01:41:55 -0500

    Added comments to Page properties and renamed 1 prop for clarity

--- app/src/main/java/com/example/quranspacedrepetition/feature_pages/domain/model/Page.kt
+++ app/src/main/java/com/example/quranspacedrepetition/feature_pages/domain/model/Page.kt
@@ -5,10 +5,15 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Page(
+    /** Page Number in the Quran */
     @PrimaryKey
     val pageNumber: Int,
+    /** Number of days to wait before the next review */
     val interval: Int = 0,
+    /** Number of continuous *correct* repetitions */
     val repetitions: Int = 0,
+    /** Easiness factor - calculated based off how easily info. is remembered */
     val eFactor: Double = 2.5,
-    val dueDate: Long,
+    /** Start time (in unix seconds) for the next day this page is due for review */
+    val dueDateUnixSecs: Long,
 )
