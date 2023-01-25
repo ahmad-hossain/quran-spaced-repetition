86a5d8d743406e1be305fe66ee313aebb1218590
Name <ahmad.awaab@gmail.com>
2023-01-25 00:20:52 -0500

    Fetched today's pages & all pages in ViewModel and added `displayedPages` state

--- app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/pages/PagesState.kt
+++ app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/pages/PagesState.kt
@@ -3,6 +3,7 @@ package com.example.quranspacedrepetition.feature_pages.presentation.pages
 import com.example.quranspacedrepetition.feature_pages.domain.model.Page
 
 data class PagesState(
+    val displayedPages: List<Page> = emptyList(),
     val allPages: List<Page> = emptyList(),
     val pagesDueToday: List<Page> = emptyList(),
     val isTodayChipSelected: Boolean = true,
--- app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/pages/PagesViewModel.kt
+++ app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/pages/PagesViewModel.kt
@@ -4,13 +4,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.quranspacedrepetition.feature_pages.domain.repository.PageRepository
 import com.example.quranspacedrepetition.feature_pages.presentation.pages.PagesEvent.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import timber.log.Timber
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
-class PagesViewModel @Inject constructor() : ViewModel() {
+class PagesViewModel @Inject constructor(
+    repository: PageRepository
+) : ViewModel() {
 
     var state by mutableStateOf(PagesState())
         private set
@@ -33,5 +40,16 @@ class PagesViewModel @Inject constructor() : ViewModel() {
                 )
             }
         }
+    }
+
+    init {
+        val currEpochDay = LocalDate.now().toEpochDay()
+        repository.getDuePagesForEpochDay(currEpochDay).onEach {
+            state = state.copy(pagesDueToday = it)
+        }.launchIn(viewModelScope)
+
+        repository.getPages().onEach {
+            state = state.copy(allPages = it)
+        }.launchIn(viewModelScope)
     }
 }
