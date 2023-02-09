644c543cfe07a51aaa627aaa7398cdd569c8163d
Name <ahmad.awaab@gmail.com>
2023-02-09 18:59:21 -0500

    Improved UI by removing start & end borders from TableCells

--- app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/components/TableCell.kt
+++ app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/components/TableCell.kt
@@ -1,15 +1,19 @@
 package com.example.quranspacedrepetition.feature_pages.presentation.components
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -26,8 +30,27 @@ fun RowScope.TableCell(
         maxLines = 1,
         overflow = TextOverflow.Ellipsis,
         modifier = modifier
-            .border(1.dp, Color.Gray)
+            .bottomBorder(1.dp, Color.Gray)
             .weight(weight)
             .padding(8.dp)
     )
 }
+
+fun Modifier.bottomBorder(strokeWidth: Dp, color: Color) = composed(
+    factory = {
+        val density = LocalDensity.current
+        val strokeWidthPx = density.run { strokeWidth.toPx() }
+
+        Modifier.drawBehind {
+            val width = size.width
+            val height = size.height - strokeWidthPx / 2
+
+            drawLine(
+                color = color,
+                start = Offset(x = 0f, y = height),
+                end = Offset(x = width, y = height),
+                strokeWidth = strokeWidthPx
+            )
+        }
+    }
+)
