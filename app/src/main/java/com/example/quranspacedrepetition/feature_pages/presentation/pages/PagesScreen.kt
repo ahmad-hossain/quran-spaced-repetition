543516d489ebbf463f908b2016225a6d5e07845b
Name <ahmad.awaab@gmail.com>
2023-02-09 20:42:09 -0500

    Removed TopAppBar & replaced FilterChips w/ Tabs

--- app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/pages/PagesScreen.kt
+++ app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/pages/PagesScreen.kt
@@ -1,6 +1,8 @@
 package com.example.quranspacedrepetition.feature_pages.presentation.pages
 
-import androidx.compose.foundation.*
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.quranspacedrepetition.R
@@ -46,8 +47,7 @@ fun PagesScreen(
 
     LaunchedEffect(Unit) {
         viewModel.scrollToIndex.collect {
-            // +2 to account for FilterChipSection and Spacer LazyColumn items
-            lazyListState.scrollToItem(it + 2)
+            lazyListState.scrollToItem(it)
         }
     }
 
@@ -69,7 +69,6 @@ fun PagesScreen(
     )
 
     Scaffold(
-        topBar = { CenterAlignedTopAppBar(title = { Text(stringResource(R.string.pages)) }) },
         floatingActionButton = {
             if (state.displayedPages.isEmpty()) return@Scaffold
             FloatingActionButton(
@@ -78,83 +77,50 @@ fun PagesScreen(
             )
         },
     ) { innerPadding ->
-        LazyColumn(
-            Modifier.padding(innerPadding),
-            contentPadding = PaddingValues(bottom = FabHeight + ScaffoldFabSpacing * 2),
-            state = lazyListState
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
         ) {
-            item {
-                FilterChipsSection()
-            }
-            item { Spacer(Modifier.height(8.dp)) }
-            stickyHeader {
-                if (state.displayedPages.isNotEmpty())
-                    TableHeader()
-            }
-            items(state.displayedPages) { page ->
-                PageItem(
-                    modifier = Modifier.clickable { viewModel.onEvent(PagesEvent.PageClicked(page)) },
-                    page = page
+            TabRow(
+                selectedTabIndex = if (state.isTodayChipSelected) 0 else 1,
+            ) {
+                Tab(
+                    selected = state.isTodayChipSelected,
+                    onClick = { viewModel.onEvent(PagesEvent.TodayChipClicked) },
+                    text = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Icon(imageVector = Icons.Outlined.Today, contentDescription = null)
+                            Spacer(Modifier.width(8.dp))
+                            Text(text = stringResource(R.string.today))
+                        }
+                    },
                 )
+                Tab(
+                    selected = state.isAllChipSelected,
+                    onClick = { viewModel.onEvent(PagesEvent.AllChipClicked) },
+                    text = { Text(text = stringResource(R.string.all)) },
+                )
+            }
+            LazyColumn(
+                contentPadding = PaddingValues(bottom = FabHeight + ScaffoldFabSpacing * 2),
+                state = lazyListState
+            ) {
+                stickyHeader {
+                    if (state.displayedPages.isNotEmpty())
+                        TableHeader()
+                }
+                items(state.displayedPages) { page ->
+                    PageItem(
+                        modifier = Modifier.clickable { viewModel.onEvent(PagesEvent.PageClicked(page)) },
+                        page = page
+                    )
+                }
             }
         }
-    }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-private fun FilterChipsSection(
-    modifier: Modifier = Modifier,
-    viewModel: PagesViewModel = hiltViewModel(),
-) {
-    val state = viewModel.state
-
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-    ) {
-        FilterChip(
-            modifier = Modifier
-                .weight(1f)
-                .height(36.dp),
-            selected = state.isTodayChipSelected,
-            onClick = { viewModel.onEvent(PagesEvent.TodayChipClicked) },
-            label = {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(end = 8.dp),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        modifier = Modifier.size(FilterChipDefaults.IconSize),
-                        imageVector = Icons.Outlined.Today,
-                        contentDescription = null
-                    )
-                    Spacer(Modifier.width(8.dp))
-                    Text(text = stringResource(R.string.today))
-                }
-            },
-        )
-        Spacer(modifier = Modifier.padding(8.dp))
-        FilterChip(
-            modifier = Modifier
-                .weight(1f)
-                .height(36.dp),
-            selected = state.isAllChipSelected,
-            onClick = { viewModel.onEvent(PagesEvent.AllChipClicked) },
-            label = {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(end = 8.dp),
-                    text = stringResource(R.string.all),
-                    textAlign = TextAlign.Center
-                )
-            },
-        )
     }
 }
 
