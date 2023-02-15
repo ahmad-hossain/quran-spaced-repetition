deb82aefc72ddf68f9c1d30dde6d21e8ef1a3d3e
Name <ahmad.awaab@gmail.com>
2023-02-15 16:10:26 -0500

    Added collapsing topappbar

--- app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/pages/PagesScreen.kt
+++ app/src/main/java/com/example/quranspacedrepetition/feature_pages/presentation/pages/PagesScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -68,7 +69,15 @@ fun PagesScreen(
         onSearchDialogDismissed = { viewModel.onEvent(PagesEvent.SearchDialogDismissed) },
     )
 
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(stringResource(R.string.revision)) },
+                scrollBehavior = scrollBehavior
+            )
+        },
         floatingActionButton = {
             if (state.displayedPages.isEmpty()) return@Scaffold
             FloatingActionButton(
