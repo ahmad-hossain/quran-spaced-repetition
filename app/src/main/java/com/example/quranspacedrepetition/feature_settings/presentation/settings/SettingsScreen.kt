e10de16ec80b28079b05925d89d1f0c6167113f7
Name <ahmad.awaab@gmail.com>
2023-02-22 21:13:37 -0500

    Added UI for settings to SettingsScreen

--- app/src/main/java/com/example/quranspacedrepetition/feature_settings/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/quranspacedrepetition/feature_settings/presentation/settings/SettingsScreen.kt
@@ -1,9 +1,14 @@
 package com.example.quranspacedrepetition.feature_settings.presentation.settings
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.quranspacedrepetition.R
@@ -11,6 +16,7 @@ import com.example.quranspacedrepetition.feature_pages.presentation.components.C
 import com.example.quranspacedrepetition.feature_pages.presentation.components.Screen
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination
@@ -42,6 +48,22 @@ fun SettingsScreen(
             )
         }
     ) { innerPadding ->
-        innerPadding
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            ListItem(
+                modifier = Modifier.clickable { viewModel.onEvent(SettingsEvent.NotificationTimeSettingClicked) },
+                headlineText = { Text(stringResource(R.string.notification_time_setting)) },
+                supportingText = { Text(state.userPreferences.notificationTime.format(
+                    DateTimeFormatter.ofPattern("hh:mm a"))) }
+            )
+            ListItem(
+                modifier = Modifier.clickable { viewModel.onEvent(SettingsEvent.NotificationTimeSettingClicked) },
+                headlineText = { Text(stringResource(R.string.quran_pages_setting)) },
+                supportingText = { Text("${state.userPreferences.startPage} - ${state.userPreferences.endPage}") }
+            )
+        }
     }
 }
