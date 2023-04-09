ab8de2a6c79f33f116ce294df319d4f2d6985969
Name <ahmad.awaab@gmail.com>
2023-04-09 18:24:38 -0400

    Replaced GradePageDialog radio-btns w/ colored bg for less clutter

--- app/src/main/java/com/github/ahmad_hossain/quranspacedrepetition/feature_pages/presentation/components/GradePageDialogOption.kt
+++ app/src/main/java/com/github/ahmad_hossain/quranspacedrepetition/feature_pages/presentation/components/GradePageDialogOption.kt
@@ -1,18 +1,24 @@
 package com.github.ahmad_hossain.quranspacedrepetition.feature_pages.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+private val IconSize = 24.dp
 
 @Composable
 fun GradePageDialogOption(
@@ -22,21 +28,29 @@ fun GradePageDialogOption(
     emoji: String,
     description: String,
 ) {
-    Row(
-        modifier
-            .clip(RoundedCornerShape(8.dp))
-            .clickable { onSelect() },
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        RadioButton(
-            selected = selected,
-            onClick = onSelect
-        )
-        Text(
-            text = emoji,
-            fontSize = 18.sp,
-        )
-        Spacer(Modifier.width(8.dp))
-        Text(description)
+    val backgroundColor = if (selected)
+        MaterialTheme.colorScheme.primary else
+        MaterialTheme.colorScheme.surfaceColorAtElevation(LocalAbsoluteTonalElevation.current)
+    CompositionLocalProvider(LocalContentColor provides contentColorFor(backgroundColor)) {
+        Row(
+            modifier
+                .clip(CircleShape)
+                .background(backgroundColor)
+                .clickable { onSelect() }
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (selected)
+                Icon(Icons.Default.Check, null)
+            else
+                Spacer(Modifier.width(IconSize))
+            Spacer(Modifier.width(8.dp))
+            Text(
+                text = emoji,
+                fontSize = 18.sp,
+            )
+            Spacer(Modifier.width(8.dp))
+            Text(description)
+        }
     }
 }
